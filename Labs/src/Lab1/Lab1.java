ad54f30fe5fe9371128d359fbb484be992f22616
He1i0s-OS <61101765+He1i0s-OS@users.noreply.github.com>
2022-10-25 11:27:36 -0700

    Added Lab1

---
+++ Labs/src/Lab1/Lab1.java
@@ -0,0 +1,25 @@
+package Lab1;
+
+public class Lab1
+{
+	public static void main(String args[])
+	{
+		boolean boo = true;
+		byte maxByte = Byte.MAX_VALUE;
+		short minShort = Short.MIN_VALUE;
+		int maxInt = Integer.MAX_VALUE;
+		long javaFlex = 3_000_000_000L;
+		float pie = Math.round(Math.PI);
+		double doubleYum = 2.0 * Math.round(Math.PI);
+		
+		System.out.println("boo = " + boo);
+		System.out.println("maxByte = " + maxByte);
+		System.out.println("minShort = " + minShort);
+		System.out.println("maxInt = " + maxInt);
+		System.out.println("javaFlex = " + javaFlex);
+		System.out.println("pie = " + pie);
+		System.out.println("doubleYum = " + doubleYum);
+		
+	}
+}
+
---
+++ Labs/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jackk
+ *
+ */
+module Labs {
+}
